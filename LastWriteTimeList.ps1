3116ebb49639c1dd21da4d4bb55942503921a203
Kuhmilch <46034390+Gamesux@users.noreply.github.com>
2024-10-11 09:19:51 +0200

    Update LastWriteTimeList.ps1
    Updated code to now show when a list was created and also separating each subsequent lists

--- LastWriteTimeList.ps1
+++ LastWriteTimeList.ps1
@@ -1,9 +1,12 @@
 $folderdates = Get-Childitem "C:\Users\Johanna\Documents\" | Where { $_.PsIsContainer -ne $true } | Sort-Object -Property LastWriteTime    #Files that arent folders are sorted by their last write time
 #$arrayname = @()   !From old code!
 #$arraydate = @()   !From old code!
-$csvfile = "C:\Users\Johanna\Documents\lastwritedate.csv"                 #Path for csv file is attached to a variable 
+$csvfile = "C:\Users\Johanna\Documents\lastwritedate.csv"                 #Path for csv file is attached to a variable
+$currentdate = Get-Date -Format "dd-MM-yyyy"							#Adding Variable for date
+$currenttime = Get-Date -Format "hh:mm"								#Adding variable for time
 If (Test-Path "$csvfile") {                                                           #check if the file above exists
-     Set-Content $csvfile "Name;Date"                                                 #The first two collumns for the csv are set to "Name" and "Date", this also resets the file, should it still have old data in it
+     #Adding an initial line of rows for the collumn Filename, Last Write Date, Time of scan followed by the date and time 
+     Add-Content $csvfile "Filename;Last Write Date;Time of Scan:;$currentdate;$currenttime"
         foreach ($file in $folderdates) {
             $writetime = $file.LastWriteTime                              #1
             $writedate = $writetime.ToString("dd-MM-yyyy")                #2 The last write time is reformatted
@@ -12,13 +15,16 @@ If (Test-Path "$csvfile") {                                                     
     #        $arraydate += "Date: $writedate"   !From old code!
             Add-Content $csvfile "$filename;$writedate"                   #The name and last write time of each file is added to the above listed collumns
             }
+     Add-Content $csvfile " ; ;"                                        #Extra empty line of rows to separate lists
 }else {
-            New-item -Path "$csvfile"                                  #If the check from Line 5 returns FALSE then the csv file is created
-            Set-Content $csvfile "Name;Date"                           #The rest of this runs exactly the same as the above If satetement
+          New-item -Path "$csvfile"                                   #If the check from Line 5 returns FALSE then the csv file is created
+          #The rest of this runs exactly the same as the above If satetement
+          Add-Content $csvfile "Filename;Last Write Date;Time of Scan:;$currentdate;$currenttime"
                 foreach ($file in $folderdates) {
                     $writetime = $file.LastWriteTime
                     $writedate = $writetime.ToString("dd-MM-yyyy")
                     $filename = $file.Name
                     Add-Content $csvfile "$filename;$writedate"
                     }
+                    Add-Content $csvfile " ; ;"
             }
