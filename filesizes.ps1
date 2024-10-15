bd7b4634fbe55bb3ac47c2f5c4b38594a219f2aa
Kuhmilch <46034390+Gamesux@users.noreply.github.com>
2024-10-15 13:51:13 +0200

    Add files via upload

---
+++ filesizes.ps1
@@ -0,0 +1,18 @@
+﻿$documentfiles = Get-Childitem "C:\Users\Johanna\Documents\" | Where { $_.PsIsContainer -ne $true } 
+$csvfile = "C:\Users\Johanna\Documents\filesizes.csv"        #Path for csv file is attached to a variable 
+If (Test-Path "$csvfile") {                                                       #check if the file above exists
+     Set-Content $csvfile " "                                               #Old Data is removed from the file
+        foreach ($file in $documentfiles) {
+            $filename = $file.Name 
+            $filesize = $file.Length
+            Add-Content $csvfile "$filename;$filesize"                 #The name and filesize of each file is added to the above listed collumns
+            }
+}else {
+            New-item -Path "$csvfile"                                  #If the check from Line 5 returns FALSE then the csv file is created
+            Set-Content $csvfile " "                           #The rest of this runs exactly the same as the above If satetement
+                foreach ($file in $documentfiles) {
+                    $filename = $file.Name
+                    $filesize = $file.Length
+                    Add-Content $csvfile "$filename;$filesize"
+                    }
+            }
